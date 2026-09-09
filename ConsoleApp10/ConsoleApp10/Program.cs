9ccc9fd1028214d0ed9a3af0ef0932f88fb0cd9f
alexsandr133 <alexsandramg65@gmail.com>
2026-09-09 22:08:19 +0500

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        class DialogWindow { 
+        
+        static void Main()
+        {
+                MessageBox.Show("предупреждение екомпьютерный вирус", "окно с названием",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Warning);
+
+        }
+        
+        
+        }
+        
+    }
+}
